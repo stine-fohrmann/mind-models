d5173b1649b29ea100c6d108cb5e99133f967362
stine-fohrmann <61832076+stine-fohrmann@users.noreply.github.com>
2021-10-22 14:19:05 +0200

    added sigmoidal model plot for different S

--- ODEsolver.py
+++ ODEsolver.py
@@ -5,51 +5,43 @@ import matplotlib.pyplot as plt
 
 
 # function that returns dy/dt
-def odes(x, t):
+def odes(x, t, s):
     # constants
     k_0 = 0.01
     k_1 = 1
-    k_2 = 5
+    k_2 = 1
     K_m1 = 0.05
     K_m2 = 0.05
-    a = 1
-    S = 2 + 0.1*np.sin(t)
+    S = s
+    # S = 2 + 0.1*np.sin(t)
 
     # assign each ODE to a vector element
     R = x[0]
-    #R_P = x[2]
-    #R_T = 1
+    R_P = x[1]
+    R_T = 1
 
-    # define each ODE
-    # sigmoidal:
-    # dRdt = k_0 + k_1*S - k_2*R
-    # dR_Pdt = (k_1*S * (R_T - R_P))/(K_m1 + R_T - R_P) - (k_2*R_P)/(K_m2+R_P)
-
-    # linear:
+    # define each ODE: sigmoidal model (c)
     dRdt = k_0 + k_1 * S - k_2 * R
-    #R_T = R + R_P
-    #dR_Pdt = k_1 * S * (R_T - R_P) - k_2 * R_P
+    dR_Pdt = (k_1 * S * (R_T - R_P)) / (K_m1 + R_T - R_P) - (k_2 * R_P) / (K_m2 + R_P)
 
-    return [dRdt]
+    return [dRdt, dR_Pdt]
 
 
 # initial condition
-x_0 = [1]
+x_0 = [1, 1]
 
-# print(odes(x=x_0, t=0))
-
-# declare a time vector
+# declare time vector
 t = np.linspace(0, 10, 100)
 
-# solve system of diff. eq.
-x = odeint(odes, x_0, t)
+# solve system of diff. eq. for 0 < S < 5, R_0=1
+S_values = range(5)
+for S in S_values:
+    x = odeint(odes, x_0, t, args=(S,))
+    R = x[:, 0]
+    R_P = x[:, 1]
+    plt.plot(t, R)
 
-R = x[:, 0]
-#R_P = x[:, 1]
-#S = x[:, 2]
-
-# plot results
-plt.plot(t, R)
-plt.xlabel('t')
-plt.ylabel('$R$')
+plt.title('$R(t)$ for different values of $S$')
+plt.xlabel('$t$')
+plt.ylabel('$R(t)$')
 plt.show()
