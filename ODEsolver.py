7bb32a0ec1c95d50e13d171f0f4eb61fc189e9d7
stine-fohrmann <61832076+stine-fohrmann@users.noreply.github.com>
2021-10-27 12:55:59 +0200

    removed dR/dt, plotted RP instead

--- ODEsolver.py
+++ ODEsolver.py
@@ -16,37 +16,31 @@ def odes(x, t, s):
     # S = 2 + 0.1*np.sin(t)
 
     # assign each ODE to a vector element
-    R = x[0]
-    R_P = x[1]
+    R_P = x[0]
     R_T = 1
 
     # define each ODE: sigmoidal model (c)
-    dRdt = k_0 + k_1 * S - k_2 * R
     dR_Pdt = (k_1 * S * (R_T - R_P)) / (K_m1 + R_T - R_P) - (k_2 * R_P) / (K_m2 + R_P)
 
-    return [dRdt, dR_Pdt]
+    return [dR_Pdt]
 
-
-# initial condition
-# R_0 = 1
-R_P0 = 1
 
 # declare time vector
-t = np.linspace(0, 6, 100)
+t = np.linspace(0, 3, 100)
 
 # solve system of diff. eq. for 0 < S < 4, R_0=1,2
 S_values = range(5)
-R_0_values = [1, 2]
+# initial condition
+R_P_values = [0, 0.1, 0.5]
 for S in S_values:
-    for R_0 in R_0_values:
-        x_0 = [R_0, R_P0]
+    for R_P0 in R_P_values:
+        x_0 = [R_P0]
         x = odeint(odes, x_0, t, args=(S,))
-        R = x[:, 0]
-        R_P = x[:, 1]
-        plt.plot(t, R, label=f'$S={S}$, ${R_0=}$')
+        R_P = x[:, 0]
+        plt.plot(t, R_P, label=f'$S={S}$, ${R_P0=}$')
 
-plt.title('$R(t)$ for different values of $S$ and $R_0$')
+plt.title('$R_P(t)$ for different values of $S$ and $R_{P0}$')
 plt.xlabel('$t$')
-plt.ylabel('$R(t)$')
+plt.ylabel('$R_P(t)$')
 plt.legend()
 plt.show()
