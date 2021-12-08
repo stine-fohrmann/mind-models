efc0051cd0078fe481fed504e24e4123e9afde32
stine-fohrmann <61832076+stine-fohrmann@users.noreply.github.com>
2021-12-08 12:25:32 +0100

    added smooth steady state graph

--- ODEsolver.py
+++ ODEsolver.py
@@ -30,58 +30,65 @@ def odes(x, t, s=0, amp=0, freq=0):
 # declare time vector
 t = np.linspace(0, 50, 100)
 
-# solve system of diff. eq. for 0 < S < 4, R_0=1,2
-# S_values = np.linspace(0, 3, 100)
+S_values = np.linspace(0, 3, 100)
 # S_values = [0, 0.5, 1, 1.5, 2]
-S_values = [1]
+# S_values = [1]
 # initial condition
-R_P_values = [0.1]
+# R_P_values = [0.1]
 # R_P_values = np.linspace(0, 1, 2)
+R_P_values = np.linspace(0, 1, 10)
 amps = [1]
 # amps = np.linspace(0, 2, 5)
 # freq = 0.1
-freqs = np.linspace(0, 1, 5)
+# freqs = np.linspace(0, 1, 5)
+freqs = [0]
 colors = ['r', 'c', 'g', 'b', 'm']
 counter2 = 0
+steadystates = []
+S_inits = []
 for freq in freqs:
     for amp in amps:
         for R_P0 in R_P_values:
             counter = 0
             for S_init in S_values:
                 x_0 = [R_P0]
-                x = odeint(odes, x_0, t, args=(S_init, amp, freq))
+                # x = odeint(odes, x_0, t, args=(S_init, amp, freq))
                 x2 = odeint(odes, x_0, t, args=(S_init, 0, 0))
-                R_P = x[:, 0]
+                # R_P = x[:, 0]
                 R_P_const = x2[:, 0]
                 # if counter < 5 and counter2 < 1:
                 #     plt.plot(t, R_P, label=f'$S={S_init}$', color=colors[counter])
                 # else:
                 #     plt.plot(t, R_P, color=colors[counter])
 
-                if R_P[-1] > R_P0:
-                    color = 'lightskyblue'
-                else:
-                    color = 'pink'
-                # amp = 0
-                # freq = 0
+                # if R_P[-1] > R_P0:
+                #     color = 'lightskyblue'
+                # else:
+                #     color = 'pink'
+                amp = 0
+                freq = 0
                 signal = S_init + amp * np.sin(freq * t)
                 # plt.plot(signal, R_P)#, color=color)
                 # plt.plot(t, R_P, label=f'$S_0 ={S_init} $', color=colors[counter])
                 # plt.plot(t, R_P, label=f'$a ={amp} $')
-                plt.plot(t, R_P, label=f'$f ={freq} $')
+                # plt.plot(t, R_P, label=f'$f ={freq} $')
                 # plt.plot(t, R_P_const, '--', color=colors[counter])
-                # plt.plot(S_init, R_P[-1], '.', color='black')
-
+                # plt.plot(S_init, R_P_const[-1], '.', color='black')
+                S_inits.append(S_init)
+                steadystates.append(R_P_const[-1])
                 counter += 1
             counter2 += 1
 
+plt.plot(S_inits[0:99], steadystates[0:99], '-', color='black')
+# plt.axvline(x=1, ymin=0.1, ymax=0.9, color='b', linestyle='dashed')
+
 # plt.title(f'$R_P(t)$ for oscillating $S = S_0 + {amp} * sin({freq}t)$ and $R_0={R_P_values[0]}$')
-plt.title(f'$R_P(t)$ for oscillating $S$ with varying frequency')
+# plt.title(f'$R_P(t)$ for oscillating $S$ with varying frequency')
 # plt.title('$R_P(t)$ for constant $S$ and different $R_{P0}$')
 # plt.title('Steady state curve for sigmoidal model')
-plt.xlabel('$t$')
-# plt.xlabel('$S$')
+# plt.xlabel('$t$')
+plt.xlabel('$S$')
 plt.ylabel('$R_P$')
 # plt.legend(loc='lower right')
-plt.legend()
+# plt.legend()
 plt.show()
