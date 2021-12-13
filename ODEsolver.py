ee9d820ee3b6b46f7fe9514ad6188b0b5a5caf77
stine-fohrmann <61832076+stine-fohrmann@users.noreply.github.com>
2021-12-13 22:06:54 +0100

    made figures prettier

--- ODEsolver.py
+++ ODEsolver.py
@@ -38,7 +38,8 @@ def goldbeter_koshland(u, v, J, K):
 
 
 # declare time vector
-t = np.linspace(0, 1000, 2000)
+# t = np.linspace(0, 1000, 2000)
+t = np.linspace(0, 10000, 2000)
 
 steadystates1 = []
 steadystates2 = []
@@ -46,22 +47,29 @@ S_inits1 = []
 S_inits2 = []
 
 
-def run_model(R_0, signal=(0, 0, 0), color=None):
+def run_model(R_0, signal=(0, 0, 0), color=None, label=None, ax=None):
     S_init = signal[0]
     amp = signal[1]
     freq = signal[2]
     x = odeint(odes, R_0, t, args=(S_init, amp, freq))
     R = x[:, 0]
     signal = S_init + amp * np.sin(freq * t)
+    if ax:
+        ax1 = ax
     # ax1.plot(t, signal)
     if not color:
         if R[-1] > R_0:
             color = 'lightskyblue'
         else:
             color = 'pink'
-    # ax1.plot(signal, R, color=color)
+    ax1.plot(signal, R, color=color, label=label)
     # ax1.plot(S_init, R[-1], '.', color='black')
-    # ax2.plot(t, R, label=f'$S={round(S_init, 2)}$')
+    # ax1.plot(t, R, label=f'$S={round(S_init, 4)}$')
+    # if not label:
+    #     ax1.plot(t, R, label=f'$S_0={S_init}$')
+    # else:
+    #     label = f'$S={S_init}$ ' + label
+    #     ax1.plot(t, R, label=label)
     # print(f'{S_init=}')
     # print(R[-1])
 
@@ -73,38 +81,89 @@ def run_model(R_0, signal=(0, 0, 0), color=None):
         steadystates2.append(R[-1])
 
 
-fig, (ax1) = plt.subplots(1)
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+# ax1.axvline(x=0.868, ymin=0.12, ymax=0.35, color='c', ls=':')
+# ax1.axvline(x=1.696, ymin=0.21, ymax=0.8, color='c', ls=':')
 
-R_0_values = np.concatenate([np.linspace(0, 0.1, 1), np.linspace(0.1, 0.3, 20), np.linspace(0.3, 1, 2)])
+R_0_values = np.concatenate([np.linspace(0, 0.1, 1), np.linspace(0.1, 0.3, 20), np.linspace(1.0, 1, 2)])
 S_init_values = np.linspace(0, 2, 100)
 for R_0 in R_0_values:
     for S_init in S_init_values:
         constant_signal = (S_init, 0, 0)
-        run_model(R_0, constant_signal)
+        run_model(R_0, constant_signal, color='white', ax=ax1)
 
-ax1.plot(S_inits1[0:85], steadystates1[0:85], '-', color='black')
-ax1.plot(S_inits1[84], steadystates1[84], '.', color='r', label='$S_{crit, 1}$')
 i = 743+14
 j = 783+31
-ax1.plot(S_inits2[i:j], steadystates2[i:j], '-', color='black')
-ax1.plot(S_inits2[i], steadystates2[i], '.', color='b', label='$S_{crit, 2}$')
+ax1.plot(S_inits2[i:j], steadystates2[i:j], '-', color='g', label='upper steady state line')
+ax1.plot(S_inits2[i], steadystates2[i], '.', color='g')#, label='$S_{crit, 1}$')
+ax1.plot(S_inits1[0:85], steadystates1[0:85], '-', color='m', label='lower steady state line')
+ax1.plot(S_inits1[84], steadystates1[84], 'x', color='m')#, label='$S_{crit, 2}$')
 
-for k in range(j-i):
-    print(f'{S_inits2[k+i]=}, {steadystates2[i+k]}')
+ax2.plot(S_inits2[i:j], steadystates2[i:j], '-', color='g', label='upper steady state line')
+ax2.plot(S_inits2[i], steadystates2[i], '.', color='g')#, label='$S_{crit, 1}$')
+ax2.plot(S_inits1[0:85], steadystates1[0:85], '-', color='m', label='lower steady state line')
+ax2.plot(S_inits1[84], steadystates1[84], 'x', color='m')#, label='$S_{crit, 2}$')
 
-print(len(S_inits2))
+ax3.plot(S_inits2[i:j], steadystates2[i:j], '-', color='g', label='upper steady state line')
+ax3.plot(S_inits2[i], steadystates2[i], '.', color='g')#, label='$S_{crit, 1}$')
+ax3.plot(S_inits1[0:85], steadystates1[0:85], '-', color='m', label='lower steady state line')
+ax3.plot(S_inits1[84], steadystates1[84], 'x', color='m')#, label='$S_{crit, 2}$')
+
+
+# for k in range(j-i):
+#     print(f'{S_inits2[k+i]=}, {steadystates2[i+k]}')
+
+# print(len(S_inits2))
 
 R_0 = 0
 S_init = 1.3
 amp = 0.6
-freq = 0.02
+freq = 0.052008
 constant_signal = (S_init, 0, 0)
-# run_model(R_0, constant_signal)
-# run_model(R_0, (S_init, amp, freq), color='green')
+# run_model(R_0, constant_signal, label='(const.)')
+run_model(R_0, (S_init, amp, freq), color='orange', label='trajectory', ax=ax1)
+freq = 0.052009
+run_model(R_0, (S_init, amp, freq), color='orange', label='trajectory', ax=ax2)
+freq = 0.05201
+run_model(R_0, (S_init, amp, freq), color='orange', label='trajectory', ax=ax3)
+
+
+# R_0 = 1
+# S_init = 1.1143
+# amp = 0.6
+# freq = 0.02
+# run_model(R_0, (S_init, amp, freq))
+# R_0 = 1
+# S_init = 1.11435
+# amp = 0.6
+# freq = 0.02
+# run_model(R_0, (S_init, amp, freq))
+# R_0 = 1
+# S_init = 1.3
+# amp = 0.6
+# freq = 0.02
+# run_model(R_0, (S_init, amp, freq))
+# R_0 = 1
+# S_init = 1.3994
+# amp = 0.6
+# freq = 0.02
+# run_model(R_0, (S_init, amp, freq))
+# R_0 = 1
+# S_init = 1.3995
+# amp = 0.6
+# freq = 0.02
+# run_model(R_0, (S_init, amp, freq))
+
 # fig.suptitle(f'R(t) for S={constant_signal[0]} and S={S_init}+{amp}sin({freq}t)')
 # fig.suptitle(f'S,R-diagram for S={S_init}+{amp}sin({freq}t)')
 ax1.set_xlabel('$S$')
+ax2.set_xlabel('$S$')
+ax3.set_xlabel('$S$')
+# ax1.set_xlabel('$t$')
 ax1.set_ylabel('$R$')
-ax1.legend()
+# ax1.legend(loc='upper left')
+ax1.set_title('b=0.052008')
+ax2.set_title('b=0.052009')
+ax3.set_title('b=0.05201')
 
 plt.show()
